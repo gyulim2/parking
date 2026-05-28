faeeefbb9770b7520b81080a3503755c5724ccf2
O-Dong <it0825@naver.com>
2026-05-28 16:57:05 +0900

    refactor: mock 제거 및 더미데이터 파일 통합

--- backend/app.py
+++ backend/app.py
@@ -27,5 +27,5 @@ def static_files(path):
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 5001))
     app.run(host="0.0.0.0", port=port, debug=False)
